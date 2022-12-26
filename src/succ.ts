7177be24dcad2886589648c0d6d1786e0d9fa87c
a bad dev <dylan@tritan.dev>
2022-12-26 07:05:15 +0000

    less ugly

--- src/succ.ts
+++ src/succ.ts
@@ -5,6 +5,11 @@ import discord from "discord.js";
 async function init(time: number) {
   let counter: number = 0;
 
+  let gigachad1: string =
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Fmelmagazine.com%2Fwp-content%2Fuploads%2F2021%2F01%2FGigachad.jpg&f=1&nofb=1&ipt=a2f36fe31703152ccaa5d1be79c603c7113453661abb8b13dcf5ef5b9a8f6860&ipo=images";
+  let gigachad2: string =
+    "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F001%2F370%2F940%2F66f.jpg&f=1&nofb=1&ipt=23da9fc4d9b75fac38d794f6f6b84381c645275fc6969217ab99b310cc7c38f3&ipo=images";
+
   console.log("[Webhook Succ] ==> Getting ready to do the fucky!");
 
   let tokens = [
@@ -30,7 +35,6 @@ async function init(time: number) {
     },
   ];
 
-  // TODO: Allow all of this shit to be set on jit on jit bc they wont have sauce nigga i on fo'nem
   tokens.forEach(async (i) => {
     console.log(`[Webhook Succ] ==> Looping through webhook tokens`, i);
 
@@ -46,8 +50,7 @@ async function init(time: number) {
       let embed = new discord.EmbedBuilder()
         .setAuthor({
           name: "better than u",
-          iconURL:
-            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F001%2F370%2F940%2F66f.jpg&f=1&nofb=1&ipt=23da9fc4d9b75fac38d794f6f6b84381c645275fc6969217ab99b310cc7c38f3&ipo=images",
+          iconURL: gigachad2 as string,
         })
         .setTitle("Yikes! im inside ur mom")
         .setDescription(
@@ -59,8 +62,7 @@ async function init(time: number) {
         .send({
           content: `Sent ${counter} messages!`,
           username: "ur mom",
-          avatarURL:
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.wp.com%2Fmelmagazine.com%2Fwp-content%2Fuploads%2F2021%2F01%2FGigachad.jpg&f=1&nofb=1&ipt=a2f36fe31703152ccaa5d1be79c603c7113453661abb8b13dcf5ef5b9a8f6860&ipo=images",
+          avatarURL: gigachad1 as string,
           embeds: [embed],
         })
         .catch((e) => {
